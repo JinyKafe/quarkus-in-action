fad6ab4d6319e7969173dc412776ae9cd6f4f4ae
JinyKafe <jankotek@atlas.cz>
2025-05-13 10:54:46 +0200

    9.3.2 Reactive messaging API

---
+++ 9_3_2/reactive-messaging-example/src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,42 @@
+package org.acme;
+
+import io.smallrye.mutiny.Multi;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+
+    @Outgoing("ticks")
+    public Multi<Long> aFewTicks() {
+        Multi<Long> tickMulti = Multi.createFrom()
+                .ticks().every(Duration.ofSeconds(1))
+                .select().first(5);
+        return tickMulti;
+    }
+
+    @Incoming("ticks")
+    @Outgoing("times")
+    public Multi<String> processor(Multi<Long> ticks) {
+        return ticks.map(tick -> Instant.now().toString());
+    }
+
+    @Incoming("times")
+    public void consumer(String payload) {
+        System.out.println(payload);
+    }
+
+}
